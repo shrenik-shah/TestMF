53876f16b8482bcb4df023e7df4a59cb8756ec2a
shrenik-shah <shrenik.shah@capgemini.com>
2016-09-07 16:58:02 +0530

    New
    New

---
+++ hello.cbl
@@ -0,0 +1,19 @@
+       identification division.
+       program-id. hello.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+       MAIN-LOGIC SECTION.
+             BEGIN.
+            display "Hello world! I am".
+            STOP RUN.
+            MAIN-LOGIC-EXIT.
+            EXIT. 
+           goback.
+           
+       end program hello.
